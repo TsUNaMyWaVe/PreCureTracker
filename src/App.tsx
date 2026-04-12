aa3c8c3765c23281866ff155c45f3618e82e0e0c
[dyad] <git@dyad.sh>
2026-04-12 14:57:13 +0900

    [dyad] Fixing AppSidebar import path - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/components/AppSidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 import Index from "./pages/Index";
 import SeasonDetail from "./pages/SeasonDetail";
 import NotFound from "./pages/NotFound";
